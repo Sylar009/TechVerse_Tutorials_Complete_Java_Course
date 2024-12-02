23daa736cdcaa0f335244f416eaa1842bff2b0b7
Sanyam Lawania <39163842+Sylar009@users.noreply.github.com>
2024-12-02 18:03:57 +0530

    Lecture 22

---
+++ Lecture 22/1/src/Demo.java
@@ -0,0 +1,9 @@
+public class Demo {
+    public static void main(String[] args) {
+        int num = 10;
+        if (num < 15) {
+            System.out.println("10 is less than 15");
+        }
+        System.out.println("Outside if-block");
+    }
+}
