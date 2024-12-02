8fbd62eae7ca3cca6fdf1904999e62d4eb8fab44
Sanyam Lawania <39163842+Sylar009@users.noreply.github.com>
2024-12-02 18:37:10 +0530

    Lecture 25

---
+++ Lecture 25/2/src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        for (int count = 1; count <= 5; count++) {
+            System.out.println("Hello World");
+        }
+    }
+}
---
+++ Lecture 25/4/src/EnhancedLoop.java
@@ -0,0 +1,8 @@
+public class EnhancedLoop {
+    public static void main(String[] args) {
+        String[] names = {"Alice", "Bob", "Charlie"};
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
